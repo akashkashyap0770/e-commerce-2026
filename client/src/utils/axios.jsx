82b8422829608f65ba904d94913a3853e1e11477
Akash Kashyap <akashkashyap0770@gmail.com>
2026-04-04 16:09:04 +0530

    add backend url

--- client/src/utils/axios.jsx
+++ client/src/utils/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://e-commerce-2026.onrender.com",
   withCredentials: true, // cookie send karne ke liye MUST
 });
 
--- server/index.js
+++ server/index.js
@@ -1,109 +1,109 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const MongodbConnected = require("./connection");
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const MongodbConnected = require("./connection");
 
-// 💠 Automatically Authentication
-const session = require("express-session");
+// // 💠 Automatically Authentication
+// const session = require("express-session");
 
-// 💠 Manually Authentication
-// const cookieParser = require("cookie-parser");
-const checkAuth = require("./middlewares/auth.middleware");
+// // 💠 Manually Authentication
+// // const cookieParser = require("cookie-parser");
+// const checkAuth = require("./middlewares/auth.middleware");
 
-const userRoutes = require("./routes/user.route");
+// const userRoutes = require("./routes/user.route");
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+// const app = express();
+// const PORT = process.env.PORT || 5000;
 
-// Middleware
-// Cors: cross origin system
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// // Middleware
+// // Cors: cross origin system
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   }),
+// );
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
 
-// Automatically Session setup
-app.use(
-  session({
-    secret: process.env.SECRETKEY,
-    resave: false, // Session ko har request pe dobara save nahi karega agar usme koi change nahi hua.
-    saveUninitialized: false, // Jab tak session me koi data store nahi hota, tab tak session create hi nahi karega.
-    cookie: {
-      httpOnly: true, // Browser me jo cookie store hoti hai, usse JavaScript (frontend code) access nahi kar sakta.
-    },
-  }),
-);
+// // Automatically Session setup
+// app.use(
+//   session({
+//     secret: process.env.SECRETKEY,
+//     resave: false, // Session ko har request pe dobara save nahi karega agar usme koi change nahi hua.
+//     saveUninitialized: false, // Jab tak session me koi data store nahi hota, tab tak session create hi nahi karega.
+//     cookie: {
+//       httpOnly: true, // Browser me jo cookie store hoti hai, usse JavaScript (frontend code) access nahi kar sakta.
+//     },
+//   }),
+// );
 
-/**-------------------------------------------------------------------------------------------------------- */
+// /**-------------------------------------------------------------------------------------------------------- */
 
-// NO CACHE (BACK BUTTON FIX)
-/*
-- no-store → browser kuch bhi save nahi karega
-- no-cache → har request server se fresh data lega
-- must-revalidate → cache use karne se pehle server check kare
-- private → sirf user ke browser ke liye (shared cache nahi)
-*/
-app.use((req, res, next) => {
-  res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
-  next();
-});
-
-// EJS setup
-app.set("view engine", "ejs");
-app.set("views", "./views");
-
-// Routes
-app.use("/", userRoutes);
-
-/**-------------------------------------------------------------------------------------------------------- */
-
-// (Protected) HOME
-app.get("/", checkAuth, (req, res) => {
-  // const user = req.query.user_name || null;
-  // if (!user) {
-  //   return res.redirect("/signup"); // 👈 default signup
-  // }
-
-  if (!req.session.user) {
-    return res.redirect("/signin");
-  }
-
-  res.render("home", { user: req.session.user.user_name });
-});
-
-LOGOUT;
-app.get("/logout", (req, res) => {
-  req.session.destroy(() => {
-    res.redirect("/signin");
-  });
-});
-
-/**-------------------------------------------------------------------------------------------------------- */
-
-// (Protected) HOME Manually
-// app.get("/", checkAuth, (req, res) => {
-//   // res.render("home", {
-//   //   user: req.user.user_name,
-//   // });
-//   res.json({ user: req.user });
+// // NO CACHE (BACK BUTTON FIX)
+// /*
+// - no-store → browser kuch bhi save nahi karega
+// - no-cache → har request server se fresh data lega
+// - must-revalidate → cache use karne se pehle server check kare
+// - private → sirf user ke browser ke liye (shared cache nahi)
+// */
+// app.use((req, res, next) => {
+//   res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
+//   next();
 // });
 
-// Server start
-const startServer = async () => {
-  try {
-    await MongodbConnected(process.env.MONGODB_URL);
+// // EJS setup
+// app.set("view engine", "ejs");
+// app.set("views", "./views");
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Server Started at PORT:${PORT}`);
-    });
-  } catch (error) {
-    console.log("Server start error:", error);
-  }
-};
+// // Routes
+// app.use("/", userRoutes);
 
-startServer();
+// /**-------------------------------------------------------------------------------------------------------- */
+
+// // (Protected) HOME
+// app.get("/", checkAuth, (req, res) => {
+//   // const user = req.query.user_name || null;
+//   // if (!user) {
+//   //   return res.redirect("/signup"); // 👈 default signup
+//   // }
+
+//   if (!req.session.user) {
+//     return res.redirect("/signin");
+//   }
+
+//   res.render("home", { user: req.session.user.user_name });
+// });
+
+// LOGOUT;
+// app.get("/logout", (req, res) => {
+//   req.session.destroy(() => {
+//     res.redirect("/signin");
+//   });
+// });
+
+// /**-------------------------------------------------------------------------------------------------------- */
+
+// // (Protected) HOME Manually
+// // app.get("/", checkAuth, (req, res) => {
+// //   // res.render("home", {
+// //   //   user: req.user.user_name,
+// //   // });
+// //   res.json({ user: req.user });
+// // });
+
+// // Server start
+// const startServer = async () => {
+//   try {
+//     await MongodbConnected(process.env.MONGODB_URL);
+
+//     app.listen(PORT, () => {
+//       console.log(`🚀 Server Started at PORT:${PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("Server start error:", error);
+//   }
+// };
+
+// startServer();
